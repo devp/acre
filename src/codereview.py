a03ebf495030120eb74cc87a9cff093fa2e58ef2
Dev Purkayastha <dev.purkayastha@gmail.com>
2025-09-10 11:08:36 -0400

    refactor: args rewriting to config lib

--- src/codereview.py
+++ src/codereview.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
-import shlex
 import sys
-from typing import Dict, Optional
 
 from cli.context import Context as CLI_Context
 from cli.parser import parse_args_from_cli
-from lib.config.config import load_config
+from lib.config.config import load_config, resolve_cmd_from_config_aliases
 from lib.review_identifier import ReviewIdentifier
 from lib.sources.git import get_current_commit_sha, get_repo_root
 from lib.state import StateManager
 
 
-def _rewrite_args_via_aliases(aliases: Optional[Dict]):
-    if aliases:
-        if len(sys.argv) > 1 and sys.argv[1] in aliases:
-            expanded = shlex.split(aliases[sys.argv[1]])
-            sys.argv = [sys.argv[0]] + expanded + sys.argv[2:]
+def _rewrite_args_via_aliases(config):
+    if len(sys.argv) > 1:
+        cmd = sys.argv[1]
+        expanded = resolve_cmd_from_config_aliases(cmd=cmd, config=config)
+        sys.argv = [sys.argv[0]] + expanded + sys.argv[2:]
 
 
 def main():
     config = load_config()
-    _rewrite_args_via_aliases(config.get("aliases"))
+    _rewrite_args_via_aliases(config)
     key = ReviewIdentifier.determine_review_id()
     state_manager = StateManager(
             repo_root=get_repo_root(),
