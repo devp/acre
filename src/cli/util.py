7f36ad136404ae61b35f9e609a8d702ba700a9b1
Dev Purkayastha <dev.purkayastha@gmail.com>
2025-09-12 12:43:27 -0400

    fix: cli yn loops if invalid inputs

--- src/cli/util.py
+++ src/cli/util.py
@@ -1,5 +1,11 @@
-def yn(prompt, default=False):
+def yn(prompt, default=False) -> bool:
+    """Asks y/n with default on empty. Loops if unexpected input."""
     ans = input(f"{prompt} [{'Y/n' if default else 'y/N'}] ").strip().lower()
     if not ans:
         return default
-    return ans == "y"
+    elif ans == "y":
+        return True
+    elif ans == "n":
+        return False
+    else:
+        return yn(prompt, default)
