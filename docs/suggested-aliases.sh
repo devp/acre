d66ec51d9dc7f40ed931821f5ae05ef9d0a9e038
Dev Purkayastha <dev.purkayastha@gmail.com>
2025-09-12 10:28:24 -0400

    doc: example of custom commands

--- docs/suggested-aliases.sh
+++ docs/suggested-aliases.sh
@@ -1,1 +1,6 @@
 alias acre=codereview
+
+# Create your own aliases for custom workflows.
+alias acre-pr-number="acre metadata | jq -r .pr_number"
+alias acre-tests-all='for x in `acre ls --raw | rg "/test_"`; do echo $x; rg "class Test|def test_" $x; done'
+alias acre-tests-diff='for x in `acre ls --raw | rg "/test_"`; do echo $x; git diff `acre metadata | jq -r .base_commit` -- $x | rg "class Test|def test_"; done'
